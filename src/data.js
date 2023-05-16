6777e99d8fbd38146547fcc1732907c6e7f7991d
Pratik Chandra <prathik.chandra7@gmail.com>
2023-05-16 12:42:52 +0530

    second commit

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const products = [
   {
     id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    title: "MOJ-403 SPORTFOAM Trenndy, bold,Stylish, breathable, lightweight gym,training Running Shoes For Men  (White, Orange)",
     price: 109.95,
     description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      "Your perfect pack for everyday use and walks in the forest",
+    category: "men's shoes",
+    image: "https://rukminim1.flixcart.com/image/832/832/xif0q/shoe/p/n/2/-original-imagg3fg5egx6zqr.jpeg?q=70",
     rating: { rate: 3.9, count: 120 },
   },
   {
     id: 2,
-    title: "Mens Casual Premium Slim Fit T-Shirts ",
-    price: 22.3,
+    title: "Mens Printed Shirts ",
+    price: 2200,
     description:
-      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      "Slim-fitting style, contrast raglan long sleeve, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     category: "men's clothing",
     image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "https://cdn-images.farfetch-contents.com/16/59/64/14/16596414_36634256_800.jpg?c=2",
     rating: { rate: 4.1, count: 259 },
   },
   {
@@ -27,17 +27,17 @@ export const products = [
     description:
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    image: "https://5.imimg.com/data5/SELLER/Default/2022/9/KT/CE/AH/43034868/men-cotton-jacket-500x500.jpeg",
     rating: { rate: 4.7, count: 500 },
   },
   {
     id: 4,
-    title: "Mens Casual Slim Fits",
+    title: "Mens Jeans",
     price: 15.99,
     description:
       "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     category: "men's clothing",
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    image: "https://tiimg.tistatic.com/fp/1/007/631/mens-casual-wear-regular-fit-ankle-length-brown-plain-100-cotton-jeans-699.jpg",
     rating: { rate: 2, count: 430 },
   },
   {
@@ -48,28 +48,28 @@ export const products = [
     description:
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    image: "https://cdn.shopclues.com/images1/thumbnails/30496/320/320/105058827-30496286-1669178147.jpg",
     rating: { rate: 4, count: 400 },
   },
   {
     id: 6,
-    title: "Solid Gold Petite Micropave ",
-    price: 168,
+    title: "Solid Gold Necklace ",
+    price: 2200,
     description:
       "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    image: "https://m.media-amazon.com/images/I/51NFD97Y8KL._UY625_.jpg",
     rating: { rate: 3, count: 70 },
   },
   {
     id: 7,
-    title: "White Gold Plated Princess",
-    price: 9.99,
+    title: "Mens Rings",
+    price: 120,
     description:
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
     category: "jewelery",
-    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
-    rating: { rate: 3, count: 400 },
+    image: "https://m.media-amazon.com/images/I/717ELvJN2HL._AC_UL1500_.jpg",
+    rating: { rate: 4, count: 400 },
   },
   {
     id: 8,
@@ -83,13 +83,13 @@ export const products = [
   },
   {
     id: 9,
-    title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-    price: 64,
+    title: "Mens Leather Wallet ",
+    price: 560,
     description:
-      "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+      "100% GENUINE LEATHER: A man's wallet should be contemporary, masculine and durable. Crafted with top grained genuine leather, Tough stitching.Hand Made by professional artisans, beautifully crafted wallet that will only get better with age. ",
     category: "electronics",
-    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-    rating: { rate: 3.3, count: 203 },
+    image: "https://m.media-amazon.com/images/I/81nmscKwWkS._SL1500_.jpg",
+    rating: { rate:4, count: 203 },
   },
   {
     id: 10,
@@ -208,7 +208,7 @@ export const products = [
 {
   id: 21,
   title: "Burberry Shirt",
-  price: 12.99,
+  price: 1200,
   description:
     "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
   category: "men's clothing",
